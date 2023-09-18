ceb4fa992e2c133a8c3820323be56bb31adabe5f
Vu Hong Duong <duongvh.dev@gmail.com>
2023-09-18 21:49:42 +0900

    feat: Create custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface GetGameResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from '../hooks/useGames';
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setErorr] = useState('');
-
-  useEffect(() => {
-    apiClient
-      .get<GetGameResponse>('/games')
-      .then((res) => setGames(res.data.results))
-      .catch((error) => setErorr(error.message));
-  });
+  const { games, error } = useGames();
 
   return (
     <>
