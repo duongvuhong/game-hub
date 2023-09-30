d8d2600a907243fb658eded102fc2668c5153127
Vu Hong Duong <duongvh.dev@gmail.com>
2023-09-30 17:26:41 +0900

    fix: Fix build errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Box, Grid, GridItem, HStack, Show } from '@chakra-ui/react';
 import NavBar from './components/NavBar';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
-import { Genre } from './hooks/useGenres';
 import PlatformSelector from './components/PlatformSelector';
-import { Platform } from './hooks/usePlatforms';
 import SortSelector from './components/SortSelector';
 import GameHeading from './components/GameHeading';
+import { Genre } from './services/genreService';
+import { Platform } from './services/platformService';
 
 export interface GameQuery {
   genre: Genre | null;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,9 @@
 import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
-import { Game } from '../hooks/useGames';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
 import Emoji from './Emoji';
+import { Game } from '../services/gameService';
 
 interface Props {
   game: Game;
