36a49fa9cb6f1a2327e82796f2bc2cbbac0c1670
Vu Hong Duong <duongvh.dev@gmail.com>
2023-10-22 13:05:03 +0900

    feat: Build game screenshot

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ export const CACHE_KEY_GENRES = ['genres'];
 export const CACHE_KEY_PLATFORMS = ['platforms'];
 export const CACHE_KEY_GAMES = ['games'];
 export const CACHE_KEY_TRAILERS = ['trailers'];
+export const CACHE_KEY_SCREENSHOTS = ['screenshots'];
---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import { CACHE_KEY_SCREENSHOTS } from '../constants';
+import APIClient from '../services/apiClient';
+import { Screenshot } from '../entities/Screenshot';
+
+const useScreenshots = (gameId: number) => {
+  const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
+
+  return useQuery({
+    queryKey: [CACHE_KEY_SCREENSHOTS, gameId],
+    queryFn: apiClient.getAll
+  });
+};
+
+export default useScreenshots;
